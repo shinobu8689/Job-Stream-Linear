e7eae96bb27bf39706a00bb224583afc17752af1
cyrus <cyruslo0829@gmail.com>
2026-02-27 14:32:40 +1100

    prototyped non-desicion making description filter

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import requests
 from datetime import datetime, timezone
 from bs4 import BeautifulSoup
 import os
+import re
 
 headers = {"User-Agent": "Mozilla/5.0"}
 
@@ -34,7 +35,41 @@ def parse_date(time_str):
     created_time = datetime.strptime(time_str, "%Y-%m-%dT%H:%M:%SZ").replace(tzinfo=timezone.utc)
     return created_time.strftime("%d/%m/%Y")
 
+def remove_corporate_fluff(section):
+    # Find culture header
+    culture_header = section.find(
+        ["strong", "b"],
+        string=lambda x: x and "culture" in x.lower()
+    )
 
+    if culture_header:
+        # Remove everything after this header
+        for tag in culture_header.find_all_next():
+            tag.decompose()
+
+        culture_header.decompose()
+
+    return section
+
+FLUFF_KEYWORDS = [
+    "equal opportunity",
+    "diversity",
+    "inclusive",
+    "apply anyway",
+    "circle back",
+    "benefits",
+    "wellbeing",
+    "parental leave",
+    "our values"
+]
+
+def remove_fluff_paragraphs(section):
+    for p in section.find_all("p"):
+        text = p.get_text().lower()
+        if any(keyword in text for keyword in FLUFF_KEYWORDS):
+            p.decompose()
+
+    return section
 
 if __name__ == "__main__":
 
@@ -45,7 +80,7 @@ if __name__ == "__main__":
         "app_key": app_key,
         "what": "developer",
         "where": "melbourne",
-        "results_per_page": 3,
+        "results_per_page": 10,
         "distance": 25
     }
 
@@ -59,7 +94,9 @@ if __name__ == "__main__":
 
 
     for job in data["results"]: # each page shows 10 results
-        # print(json.dumps(job, indent=4))
+
+        print(f"Found {len(data['results'])}/{data['count']} job listings for '{params['what']}' in '{params['where']}' within {params['distance']}km radius:\n")
+        #print(json.dumps(job, indent=4))
         contract_time = job.get('contract_time') or "N/A"
         days_ago = f"{days_to_today(job['created'])} days ago" if days_to_today(job['created']) > 0 else "Today"
 
@@ -69,13 +106,25 @@ if __name__ == "__main__":
         soup = BeautifulSoup(page.text, "html.parser")
 
         description = soup.select_one("section.adp-body")
+        # description.get_text() for no html tags
+
+        # reducing token
+
+        # non-desicion making description filter, might not work if the description is a blob on words.
+        description = remove_corporate_fluff(description)
+        description = remove_fluff_paragraphs(description)
+        cleaned_text = description.get_text(separator="\n", strip=True)
+        # add condition check: if description no longer include the requirement, mark down for manual review
+        # basic filtering for "X years experience", certification requirement, specific tech stack requirement, etc.
 
         print(f"● {job['title']} ({contract_time}) - {job['company']['display_name']} @{job['location']['display_name']} - {parse_date(job['created'])} ({days_ago})")
-        print(f"  - Category: {job['category']['label']} - {job_url}")
-        print(f"  - Description: {description.get_text() if description else 'N/A'}")
-        print(f"\n")
+        print(f"  - Category: {job['category']['label']} - {job_url} ID: {job['id']}")
+        print(f"  - Filtered Description: {cleaned_text if cleaned_text else 'N/A'}")
+        input("\nPress Enter to continue to the next job listing...\n")
 
-        # seperate marketing stuff in description, e.g. "About the company", "What you will do", "What we are looking for", "Benefits"
-        # reducing token required and pass it to Local LLM
+        os.system('cls' if os.name == 'nt' else 'clear')
+       
+        
+        # reducing token text pass to Local LLM
         # domain, responsibilities, tech_stack, other
     
--- readme.md
+++ readme.md
@@ -1,1 +1,8 @@
 basic prototype and plannings for Job Stream Linear (Temp Name)
+
+changelog
+
+- 27/02
+  - prototyped non-desicion making description filter
+- 25/02
+  - init commit
